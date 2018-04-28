4c75c535dd869848405b0f6f8ca2db31d4d13c27
fdanielsa <dsolis@hub-camp.mx>
2018-04-28 15:41:18 -0500

    resultado final

--- js/particule.js
+++ js/particule.js
@@ -1,3 +1,6 @@
+Math.distance = function(a, b){
+	return Math.sqrt( Math.pow((a.x-b.x), 2) + Math.pow((a.y-b.y), 2) );
+}
 var canvas, context;
 var maxWidth, maxHeight;
 var time = new Date().getTime();
@@ -44,16 +47,35 @@ function animate() {
 //todas las clases llevan una funcion dentro.
 var Particle = function(args) {
 	if (args === undefined) args = {};
-	this.x = args.x || (Math.random() * window.innerWidth);
-	this.y = args.y || (Math.random() * window.innerHeight);
-	this.radius = args.radius || (Math.random() * 7);
+	this.position = {
+		x: args.x || (Math.random() * window.innerWidth),
+		y: args.y || (Math.random() * window.innerHeight)
+	}
+	this.alpha = Math.random();
+	this.rgba = "rgba(255,255,255,"+this.alpha+")";
+	this.velocity = { 
+		x: (Math.random() * 4) -2,
+		y: (Math.random() * 4) -2
+	};
+	this.radius = args.radius || this.alpha * 3;
 	//se renderizo el context
 	this.draw = function(ctx) {
+		this.update();
 		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.radius, PI2, false);
-		ctx.stroke();
+		ctx.arc(this.position.x, this.position.y, this.radius, PI2, false);
+		ctx.fillStyle = this.rgba;
 		ctx.fill();
 		ctx.closePath();
+	}
+	//update actualiza la posicion en x
+	this.update = function() {
+		this.position.x += this.velocity.x;
+		this.position.y += this.velocity.y;
+
+		if(this.position.x < 0) this.position.x = canvas.width;
+		if(this.position.y < 0) this.position.y = canvas.height;
+		if(this.position.x > canvas.width) this.position.x = 0;
+		if(this.position.y > canvas.width) this.position.y = 0;
 	}
 	return this;
 
@@ -63,11 +85,23 @@ function render(){
 	//draw mehods
 	context.clearRect(0,0, maxWidth, maxHeight);
 	for (var i = 0; i < particles.length; i++) {
-		var particle = particles[i];
-		//se cambio context por draw
-		particle.draw(context);
-	};
-
+		var a = particles[i];
+		a.draw(context);
+		for (var j = i + 1; j < particles.length; j++) {
+			var b = particles[j];
+			var distance = Math.distance(a.position, b.position);
+			if(distance < 30) {
+				context.beginPath();
+				context.moveTo(a.position.x, a.position.y);
+				context.lineTo(b.position.x, b.position.y);
+				context.stroke();
+				context.lineWidth = 1 - (distance/30);
+				
+				context.strokeStyle = "white";
+				context.closePath();
+			}
+		}
+	}
 }
 
 init();
